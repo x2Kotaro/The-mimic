7860812ab3bcbd0ef2a51cdf7d25070fbefb2886
For4za511 <93924562+For4za511@users.noreply.github.com>
2021-12-26 16:25:24 +0700

    Update WHITELIST.lua

--- WHITELIST.lua
+++ WHITELIST.lua
@@ -1,7 +1,7 @@
 local key = _G.Key
 local check = "https://sharkxdd002.000webhostapp.com/check.php?key=" .. key
 if game:HttpGet(check) == "Whitelisted" then
-loadstring(game:HttpGet("loadstring(game:HttpGet("https://raw.githubusercontent.com/For4za511/The-mimic/main/Themimic.lua"))()"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/For4za511/The-mimic/main/Themimic.lua"))()
 else
 game.Players.LocalPlayer:Kick("โปรดซื้อ script ทาง Discord ด้วยครับ")
 end
